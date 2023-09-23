37b1960ed4754d1a90fca0833488028311336565
David Huckebrink <david.huckebrink@ruhr-uni-bochum.de>
2023-09-23 13:56:31 -0700

    refactored and added tpb as decision strategy

--- components/agent.py
+++ components/agent.py
@@ -4,6 +4,7 @@ from components.technologies import HeatingTechnology
 
 from decision_making.mcda import calc_score, normalize
 
+
 class MoneyAgent(mesa.Agent):
     """An agent with fixed initial wealth."""
 
@@ -15,7 +16,7 @@ class MoneyAgent(mesa.Agent):
         installed_heating_tech,
         installed_pv_cap=0,
         interactions_per_step=1,
-        step_length_in_years=1/4
+        step_length_in_years=1 / 4,
     ):
         # Pass the parameters to the parent class.
         super().__init__(unique_id, model)
@@ -25,12 +26,13 @@ class MoneyAgent(mesa.Agent):
         self.interactions_this_step = 0
         self.interactions_per_step = interactions_per_step
         self.step_length_in_years = step_length_in_years
-        self.disposable_income = disposable_income*step_length_in_years
+        self.disposable_income = disposable_income * step_length_in_years
         self.heating_tech = installed_heating_tech
         available_techs = self.model.heating_techs_df.index
         self.tech_attitudes = dict(
             zip(available_techs, np.random.random(len(available_techs)))
         )
+        self.pbc = self.random.random()
 
     def step(self):
         """called each `step´ of the model.
@@ -38,10 +40,13 @@ class MoneyAgent(mesa.Agent):
         self.heating_tech.age += self.step_length_in_years
         self.interactions_this_step = 0
         self.interact()
-        self.wealth += self.disposable_income - self.heating_tech.total_cost_per_year(20_000)
+        self.wealth += (
+            self.disposable_income - self.heating_tech.total_cost_per_year(20_000)
+        ) * self.step_length_in_years
 
         if self.heating_tech.age > self.heating_tech.lifetime:
-            self.purchase_new_heating()
+            self.purchase_heating_tbp_based()
+            # self.purchase_new_heating()
 
     def interact(self):
         """interaction with other agents.
@@ -51,12 +56,15 @@ class MoneyAgent(mesa.Agent):
         neighbours = self.model.grid.get_neighbors(self.pos, moore=True, radius=2)
         if len(neighbours) > 1:
             other = self.random.choice(neighbours)
-            if any(x.interactions_this_step >= x.interactions_per_step for x in [self, other]):
+            if any(
+                x.interactions_this_step >= x.interactions_per_step
+                for x in [self, other]
+            ):
                 return
 
             for tech in self.tech_attitudes.keys():
                 att_diff = self.tech_attitudes[tech] - other.tech_attitudes[tech]
-                
+
                 att_diff *= 1 - abs(self.tech_attitudes[tech])
 
                 self.tech_attitudes[tech] += att_diff * 0.01
@@ -66,27 +74,47 @@ class MoneyAgent(mesa.Agent):
         else:
             # no neighbours to interact with
             return
-        
-    
+
     def purchase_new_heating(self):
         techs_df = self.model.heating_techs_df
         techs_df["attitude"] = self.tech_attitudes
         techs_df["attitude"] = normalize(techs_df["attitude"] + 1)
         # calculate scores
-        techs_df["total_score"] = techs_df[["emissions[kg_CO2/a]_norm","total_cost[EUR/a]_norm", "attitude"]]\
-                .apply(
-                    calc_score, 
-                    axis=1, 
-                    weights={"emissions[kg_CO2/a]_norm":0.3,"total_cost[EUR/a]_norm":0.5, "attitude":0.2}
-                    )
+        techs_df["total_score"] = techs_df[
+            ["emissions[kg_CO2/a]_norm", "total_cost[EUR/a]_norm", "attitude"]
+        ].apply(
+            calc_score,
+            axis=1,
+            weights={
+                "emissions[kg_CO2/a]_norm": 0.3,
+                "total_cost[EUR/a]_norm": 0.5,
+                "attitude": 0.2,
+            },
+        )
         best_tech_idx = techs_df["total_score"].argmin()
-        new_tech = techs_df.iloc[best_tech_idx,:]
-        
+        new_tech = techs_df.iloc[best_tech_idx, :]
+
         # TODO: implement affordability constraint
-        
+
         self.heating_tech = HeatingTechnology.from_series(new_tech)
-        
         pass
+
+    def purchase_heating_tbp_based(self):
+        techs_df = self.model.heating_techs_df
+        for tech_name, tech_att in self.tech_attitudes.items():
+            # TODO: at least a sensitivity analysis for arbitrary value
+            if tech_att > 0.7:
+                annual_cost = techs_df.loc[tech_name, "total_cost[EUR/a]"]
+                if self.disposable_income > annual_cost:
+                    random_pbc = self.random.random()
+                    # TODO: this might lead to the situation in which the lifetime of 
+                    # an appliance has expired, but due to lacking pbc, no new appliance
+                    # is being bought
+                    if random_pbc < self.pbc:
+                        self.heating_tech = HeatingTechnology.from_series(
+                            techs_df.loc[tech_name, :]
+                        )
+                        return
 
     def choose_new_heating_sys(self):
         """should be called as a function of remaining tech lifetime?"""
